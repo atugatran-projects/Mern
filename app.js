0d35d98e9215ea2f55d756d246bf3b39b619d352
Atugatran <atugatran@gmail.com>
2023-01-01 14:21:08 +0530

    Improve dockerfile

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const mongoose = require("mongoose");
 require('dotenv').config()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // Database Connection
 require("./Database/db_conn");
